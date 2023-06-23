e364f0c148bd0ad8922e5fda51760621d92f46e4
Zhan Teyvans <teyvans@gmail.com>
2023-06-23 12:14:33 +0300

    Destroed1 test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
                 .post("/post")
 // Проверки
                 .then()
-                .statusCode(200)
+                .statusCode(220)
                 .contentType(ContentType.JSON)
                 .body("data", equalTo("some data"))
         ;
